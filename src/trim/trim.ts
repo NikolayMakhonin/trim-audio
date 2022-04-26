6aa66d853f273306e914598f69ae1da56d028531
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2022-04-26 12:45:03 +0200

    develop

--- src/trim/trim.ts
+++ src/trim/trim.ts
@@ -570,6 +570,52 @@ export function trimAudio({
   // }
 }
 
+export function smoothAudio({
+  samplesData,
+  channelsCount,
+  startSamples,
+  endSamples,
+}: {
+  samplesData: Float32Array,
+  channelsCount: number,
+  startSamples: number,
+  endSamples: number,
+}) {
+  if (!startSamples && !endSamples) {
+    return
+  }
+  if (!startSamples) {
+    startSamples = 0
+  }
+  if (!endSamples) {
+    endSamples = 0
+  }
+
+  const samplesCount = Math.floor(samplesData.length / channelsCount)
+  if (startSamples + endSamples > samplesCount) {
+    const mult = samplesCount / (startSamples + endSamples)
+    startSamples *= mult
+    endSamples *= mult
+  }
+
+  if (startSamples) {
+    for (let i = 0; i < startSamples; i++) {
+      const mult = i / startSamples
+      for (let channel = 0; channel < channelsCount; channel++) {
+        samplesData[i * channelsCount + channel] *= mult
+      }
+    }
+  }
+  if (endSamples) {
+    for (let i = 0; i < endSamples; i++) {
+      const mult = i / endSamples
+      for (let channel = 0; channel < channelsCount; channel++) {
+        samplesData[(samplesCount - 1 - i) * channelsCount + channel] *= mult
+      }
+    }
+  }
+}
+
 // normalize(samples, 0.95)
 // samples = await trimSamples({
 //     samples,
